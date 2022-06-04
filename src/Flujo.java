6da40fb962c61a9064a255702bf05ad56b841316
Cristian <veracristian89@gmail.com>
2022-06-04 10:52:17 -0300

    extendiendo excepcion

--- src/Flujo.java
+++ src/Flujo.java
@@ -1,25 +1,30 @@
+import javax.sound.midi.MidiUnavailableException;
 
 public class Flujo {
 
 	public static void main(String[] args) {
 		System.out.println("inicio main");
-		metodo1();
+		try {
+			metodo1();
+				// atrapa todas las excepciones que extienden de exception.
+		} catch ( Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		System.out.println("fin main");
 	}
 	
-	private static void metodo1() {
+	private static void metodo1() throws MiExcepcion {
 		System.out.println("inicion metodo1");
-		try {
 			metodo2();
-		} catch(MiExcepcion me) {
-			me.printStackTrace();
-		}
-		
 		System.out.println("fin metodo1");
 	}
 	
 	private static void metodo2() throws MiExcepcion {
 		System.out.println("inicion metodo2");
+		int a = 50/0;
+		Cuenta c = null;
+		c.deposita();
 		throw new MiExcepcion("Mi excepcion fue lanzada");
 	}
 }
--- src/TestCuentaExceptionCheck.java
+++ src/TestCuentaExceptionCheck.java
@@ -1,4 +1,14 @@
 
 public class TestCuentaExceptionCheck {
 
+	public static void main(String[] args) {
+		Cuenta cuenta = new Cuenta();
+		try {
+			cuenta.deposita();
+		} catch (MiExcepcion e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 }
